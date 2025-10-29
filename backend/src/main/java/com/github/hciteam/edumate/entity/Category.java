70915b5a01c147993625e0acc3cc89e6040e065b
TarekSaeed0 <naminode2020@gmail.com>
2025-10-29 22:37:18 +0300

    feat: added FAQ and categories implementation

---
+++ backend/src/main/java/com/github/hciteam/edumate/entity/Category.java
@@ -0,0 +1,35 @@
+package com.github.hciteam.edumate.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String name;
+
+	@ManyToMany(mappedBy = "categories")
+	private Set<FAQ> faqs = new HashSet<>();
+}
