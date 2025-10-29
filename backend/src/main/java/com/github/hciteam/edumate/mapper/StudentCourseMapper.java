97c5c7269a7d15bdbb9e422e41367a5b0ae5485d
TarekSaeed0 <naminode2020@gmail.com>
2025-10-29 22:23:00 +0300

    feat: added dtos and mappers for mentities

---
+++ backend/src/main/java/com/github/hciteam/edumate/mapper/StudentCourseMapper.java
@@ -0,0 +1,15 @@
+package com.github.hciteam.edumate.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import com.github.hciteam.edumate.entity.StudentCourse;
+import com.github.hciteam.edumate.model.StudentCourseDTO;
+
+@Mapper(componentModel = "spring", uses = {SemesterCourseMapper.class})
+public interface StudentCourseMapper {
+	@Mapping(source = "student.id", target = "studentId")
+	StudentCourseDTO toDTO(StudentCourse studentCourse);
+
+	@Mapping(source = "studentId", target = "student.id")
+	StudentCourse toEntity(StudentCourseDTO studentCourseDTO);
+}
---
+++ backend/src/main/java/com/github/hciteam/edumate/model/SemesterCourseDTO.java
@@ -0,0 +1,14 @@
+package com.github.hciteam.edumate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SemesterCourseDTO {
+	private Long id;
+	private Long semesterId;
+	private CourseDTO course;
+}
